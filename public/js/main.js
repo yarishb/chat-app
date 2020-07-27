ec39c231d8be48e90fcad6c9b6a7fdab14d2d3ae
yarishb <bogdan040105@gmail.com>
2020-07-27 23:21:20 +0300

    final version

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ const chatMessages = document.querySelector(".chat-messages")
 const roomName = document.getElementById("room-name")
 const userList = document.getElementById("users")
 
+
 // Get username and room 
 const { username, room } =  Qs.parse(location.search, {
     ignoreQueryPrefix: true
